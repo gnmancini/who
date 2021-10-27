2d74a844def0a3cde43af118babb7d3818784f15
Genaro Mancini <genaro.mancini@ns90.ie>
2021-10-27 10:16:44 +0100

    changes for the first version

--- who.py
+++ who.py
@@ -5,7 +5,7 @@ import dbus
 def chequeo():
     loop = True
     while (loop):  
-        result = os.popen('w | grep -v $itsme | tail -n +3 | wc -l').read()
+        result = os.popen('w | tail -n +3 | wc -l').read()
         #print(result)
         if int(result) >= 1 :
             print(result)
@@ -17,12 +17,11 @@ def intruder_message():
     notify_object = bus.get_object('org.freedesktop.Notifications','/org/freedesktop/Notifications')
     notify_interface = dbus.Interface(notify_object,'org.freedesktop.Notifications')
     titulo = "IMPORTANT!!!"
-    cuerpo = "There is a different user logged in the system using ssh."
+    cuerpo = "There are more than one user logged in the system."
     tiempo = 10000 # milisegundos. Si especificamos 0, se espera a que el usuario lo cierre
     notify_id = notify_interface.Notify("DBus Test", 0, "gtk-ok", titulo, cuerpo, "", {}, tiempo)
  
     # Imprimimos el ID de esta notificacion
     #print ("El ID es: ", notify_id)
 
-itsme = os.popen('whoami').read()
 chequeo()
