fec63524f466c5a461fca7b407afe52cc4aecf63
Genaro Mancini <genaro.mancini@ns90.ie>
2021-10-27 10:27:28 +0100

    Updating rule to check users

--- who.py
+++ who.py
@@ -7,7 +7,7 @@ def chequeo():
     while (loop):  
         result = os.popen('w | tail -n +3 | wc -l').read()
         #print(result)
-        if int(result) >= 1 :
+        if int(result) > 1 :
             print(result)
             intruder_message()
         time.sleep(60)
